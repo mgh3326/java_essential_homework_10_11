141b91c7605438801af2e89cd280db410e1c4c76
mgh3326 <mgh3326@naver.com>
2017-11-13 16:51:42 +0900

    submit complete

--- src/ImageSizeChange.java
+++ src/ImageSizeChange.java
@@ -1,49 +1,49 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class ImageSizeChange extends JFrame {
-    ImageIcon icon = new ImageIcon("images/image0.jpg");// 이미지 로딩
+    ImageIcon icon = new ImageIcon("img.jpg");// 이미지 로딩
     Image img = icon.getImage(); // 이미지 객체
     int width = img.getWidth(this);//이미지의 폭, 픽셀 단위
-    int height = img.getWidth(this);//이미지의 높이, 픽셀 단위
+    int height = img.getHeight(this);//이미지의 높이, 픽셀 단위
+
     ImageSizeChange() {
-        setTitle("원본 크기로 원하는 위치에 이미지 그리기");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("그래픽 이미지 10% 확대 축소");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//View 종료시에 프로그램도 종료되도록 합니다.
         setContentPane(new MyPanel());
-
-        setSize(300, 400);
+        setSize(400, 400);//사이즈는 400*400으로 하였습니다.
         setVisible(true);
+        //getContentPane().requestFocus();
     }
-
-    class MyPanel extends JPanel {
-
+    class MyPanel extends JPanel
+    {
+        public MyPanel()
+        {
+            addKeyListener(new KeyAdapter()
+            {
+                public void keyPressed(KeyEvent e)
+                {
+                    if(e.getKeyChar() == '+')//+키를 입력하면 이미지를 10% 확대합니다.
+                    {
+                        width = (int)(width+(width*0.1));
+                        height = (int)(height+(height*0.1));
+                        repaint();
+                    }
+                    else if(e.getKeyChar() == '-')//-키를 입력하면 이미지를 10% 축소합니다.
+                    {
+                        width = (int)(width-(width*0.1));
+                        height = (int)(height-(height*0.1));
+                        repaint();
+                    }
+                }
+            });
+        }
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-
             // 이미지를 패널의(10,10)에 원래의 크기로 그린다.
-            g.drawImage(img, 10, 10, this);
-            super.addKeyListener(new MyKeyListener());
+            g.drawImage(img, 10, 10,width,height, this);//10,10 위치에 원본 크기로 출력합니다.
             super.requestFocus();	// 컨텐트팬이 키 입력을 받을 수 있도록 포커스 강제 지정
-
-
-
-
-        }
-    }
-    class MyKeyListener extends KeyAdapter { // Key 리스너 구현
-        public void keyPressed(KeyEvent e) {
-            int keyCode = e.getKeyCode(); // 입력된 키의 키코드를 알아낸다.
-
-            switch (keyCode) { // 키 코드에 따라 상,하,좌,우 키 판별, 레이블의 위치 이동
-                case KeyEvent.VK_UP:  //UP 키
-                    repaint(10, 10, 5, 5);
-                    break;
-                case KeyEvent.VK_DOWN:  //UP 키
-                    repaint(10, 10, width / 2, height / 2);
-                    break;
-            }
         }
     }
     public static void main(String [] args) {
--- src/RandomNumber_Game.java
+++ src/RandomNumber_Game.java
@@ -1,26 +1,53 @@
-import java.awt.*;  import javax.swing.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
 public class RandomNumber_Game extends JFrame {
+    JLabel [] numLab = new JLabel[10];
+    Random ran = new Random();
+    int x, y;
+    boolean [] selectState = new boolean[10];
     RandomNumber_Game(){
-        setTitle("Random Labels");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Container contentPane = getContentPane(); // 컨텐트팬 알아내기
+        setTitle("Ten 레이블 클릭");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        for(int i = 0; i < selectState.length; i++)
+            selectState[i] = false;//선택이 되면 false가 되도록 하였습니다.
+       Container contentPane = getContentPane(); // 컨텐트팬 알아내기
         contentPane.setLayout(null);
-        for(int i=0;i<10;i++){
-            JLabel label = new JLabel();//레이블 컴포넌트 생성
-            int x = (int)(Math.random()*200) + 50;//50~200의 랜덤 x
-            int y = (int)(Math.random()*200) + 50;//50~200의 랜덤 y
-            label.setLocation(x,y);//label을 (x,y)에 배치
-            label.setSize(10,10);//label 크기를 10x10으로 설정
-            label.setOpaque(true);//label의 배경색이 보이게함
-            label.setText(Integer.toString(i));
-//            label.setForeground(Color.PINK);
-//            label.setBackground(Color.BLUE);//색 파랑
-            contentPane.add(label);//컨테이너에 라벨을 담음
-        };
-        setSize(300,300);// 프레임 크기 500x200 설정
+        for(Integer i=0;i<10;i++){
+            // 15 * 15 크기로  0에서 9까지 숫자를 가진 레이블 컴포넌트를 10개 만들고 이들을 프레임 내의 랜덤한 위치에 배치한다.
+            numLab[i] = new JLabel(i.toString());
+            x = (int)(Math.random()*350);//0~350의 랜덤 x
+            y = (int)(Math.random()*270);//0~270의 랜덤 y
+            numLab[i].setBounds(x, y, 15, 15);//크기를 15 * 15로 하였습니다.
+            numLab[i].setForeground(Color.MAGENTA);//색상은 마젠타로 하였습니다.
+            numLab[i].addMouseListener(new MyMouseListener());
+            contentPane.add(numLab[i]);
+        }
+        setSize(400,400);// 프레임 크기 500x200 설정
         setVisible(true);//프레임을 화면에 출력
+    }
+    class MyMouseListener extends MouseAdapter {
+        public void mousePressed (MouseEvent e) {
+            int i;
+            for(i = 0; i < selectState.length; i++)//차례대로 클릭되기 위해 사용하였습니다.
+                if(!selectState[i])//selectState[i]==false
+                    break;
+            if(e.getSource().equals(numLab[i])) {//클릭 되어진다면 숫자가 안보여지도록 하였습니다.
+                numLab[i].setVisible(false);
+                selectState[i] = true;
+            }
+            if(i == 9)//9개의 숫자를 모두 맞힌 경우 처음처럼 새로 숫자를 만들도록 하였습니다.
+                for(int j = 0; j < selectState.length; j++) {
+                    x = (int)(Math.random()*350);//0~350의 랜덤 x
+                    y = (int)(Math.random()*270);//0~270의 랜덤 y
+                    numLab[j].setLocation(x, y);
+                    numLab[j].setVisible(true);
+                    selectState[j] = false;
+                }
+        }
     }
     public static void main(String[] args){
         new RandomNumber_Game();
